baa952de64931d12affd2e116dcd6ceb48ef9a8b
Deploy from CI <>
2025-10-24 12:44:32 +0000

    Deploy 6a3e7bb2c38c28a484bc9889fa1f11005e0ed399 to deployed-pages

---
+++ 05_threads_promises/examples.ts
@@ -0,0 +1,36 @@
+function example0() {
+  let toReturn = 0
+  setTimeout(() => {toReturn = 100}, 5000)
+  return toReturn
+}
+console.log(example0())
+
+
+
+
+
+
+function example1(){
+    let toReturn = 0
+    setTimeout(() => {toReturn = 500}, 0)
+    setTimeout(() => {toReturn = 100}, 5000)
+    return toReturn
+}
+// console.log(example1())
+
+
+
+
+
+
+
+
+
+function example2(){
+    setTimeout(() => {console.log('A')}, 0)
+    setTimeout(() => {console.log('B')}, 5000)
+    console.log('C')
+    while(0 == 0) {}
+    console.log('D')
+}
+// example2()
